0030f437955183d9c3a098c9d010fcffb492db87
Mohammad Saheer Padinharayil <saheerpadinharayil@gmail.com>
2021-03-15 00:26:07 -0500

    Operations

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -21,5 +21,20 @@ public class Main {
         result = result % 3; //reminder of (4%3)
         System.out.println("4 % 3 = " + result);
 
+        // result = result +1;
+        result++;// 1+1=2
+        System.out.println("1 + 1 = " + result);
+
+        //result = result*10;
+        result*=10;// 2*10 =20
+        System.out.println("2 * 10 = " + result);
+
+        //result = result/5;
+        result/=5;// 20/5 =4
+        System.out.println("20 / 5 = " + result);
+
+        //result = result-2;
+        result-=2;// 4-2 =2
+        System.out.println("4-2 = " + result);
     }
 }
