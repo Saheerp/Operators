f1c809d0b961c3b3662632f4c15fc8371fc009cf
Mohammad Saheer Padinharayil <saheerpadinharayil@gmail.com>
2021-03-15 00:44:58 -0500

    Operators - if then statements

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,19 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int result =  3; //1+2=3
-        System.out.println("1 + 2 ="+ result);
+        int result = 3; //1+2=3
+        System.out.println("1 + 2 =" + result);
         int previousResult = result;
         System.out.println("previousResult = " + previousResult);
-        result =result-1;
+        result = result - 1;
         System.out.println(" 3 - 1 = " + result);
         System.out.println("previousResult = " + previousResult);
 
-        result = result * 10 ; // 2*10 =20
+        result = result * 10; // 2*10 =20
         System.out.println("2*10 = " + result);
 
         result = result / 5;
-        System.out.println("20 / 5 = "+ result);
+        System.out.println("20 / 5 = " + result);
 
         result = result % 3; //reminder of (4%3)
         System.out.println("4 % 3 = " + result);
@@ -26,15 +26,33 @@ public class Main {
         System.out.println("1 + 1 = " + result);
 
         //result = result*10;
-        result*=10;// 2*10 =20
+        result *= 10;// 2*10 =20
         System.out.println("2 * 10 = " + result);
 
         //result = result/5;
-        result/=5;// 20/5 =4
+        result /= 5;// 20/5 =4
         System.out.println("20 / 5 = " + result);
 
         //result = result-2;
-        result-=2;// 4-2 =2
+        result -= 2;// 4-2 =2
         System.out.println("4-2 = " + result);
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("And I'm scared of aliens");
+
+        }
+
+    int topScore = 80;
+        if(topScore <= 100) {
+            System.out.println("You got the high score!");
+
+        }
+
+        int secondTopScore =81;
+        if (topScore>secondTopScore && topScore<100)
+            System.out.println("Greater than Second top score and less than 100");
+
     }
 }
